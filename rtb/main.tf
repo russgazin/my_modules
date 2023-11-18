6f09fd823e6a2c510bcb64c0d7a81a318a2276f3
russ gazin <russgazin@gmail.com>
2023-11-18 12:53:08 -0600

    built rtb module

---
+++ rtb/main.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "rtb" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = var.gateway_id == null ? null : var.gateway_id
+    nat_gateway_id = var.nat_gateway_id == null ? null : var.nat_gateway_id
+  }
+
+  tags = {
+    Name = "${var.gateway_id == null ? "private" : "public"}_rtb"
+  }
+}
---
+++ rtb/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {
+  
+}
+
+variable "gateway_id" {
+  
+}
+
+variable "nat_gateway_id" {
+  
+}
+
