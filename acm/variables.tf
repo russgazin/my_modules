f04fb2ff89d4f0ca572def8ab54400fc3463d05a
russ gazin <russgazin@gmail.com>
2023-11-18 13:51:26 -0600

    built acm module

---
+++ acm/variables.tf
@@ -0,0 +1,19 @@
+variable "domain_name" {
+  
+}
+
+variable "subject_alternative_names" {
+  
+}
+
+variable "validation_method" {
+  
+}
+
+variable "cert_tag" {
+  
+}
+
+variable "zone_id" {
+  
+}
