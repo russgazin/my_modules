def39e9152f66378e572ad42b7904d0f4a5ed70f
Russ Gazin <88696160+russgazin@users.noreply.github.com>
2023-11-29 20:38:47 -0600

    Update main.tf for sg

--- instance/main.tf
+++ instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.vpc_security_group_ids]
   subnet_id              = var.subnet_id
   user_data              = var.user_data
 
